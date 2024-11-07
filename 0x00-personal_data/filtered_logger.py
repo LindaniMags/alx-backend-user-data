1a57560733c8eb478f55939a0eb63262c4874152
LindaniMags <lindanimags@gmail.com>
2024-11-07 13:05:09 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,29 @@
 Regex-ing
 """
 import re
+import logging
 from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT_FIELDS = ('name', 'levelname', 'asctime', 'message')
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """formats a LogRecord.
+        """
+        rec = super(RedactingFormatter, self).format(record)
+        f_rec = filter_datum(self.fields, self.REDACTION, rec, self.SEPARATOR)
+        return f_rec
 
 
 patterns = {
