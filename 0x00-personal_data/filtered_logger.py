5bbb23e602fb020f883d45f043f210741ed4a007
LindaniMags <lindanimags@gmail.com>
2024-11-07 17:37:36 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -66,9 +66,10 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     returns a connector to the database
     """
     mydb = mysql.connector.connect(
-        host=os.environ.get("PERSONAL_DATA_DB_HOST", "localhost"),
         user=os.environ.get("PERSONAL_DATA_DB_USERNAME", "root"),
         password=os.environ.get("PERSONAL_DATA_DB_PASSWORD", ""),
+        host=os.environ.get("PERSONAL_DATA_DB_HOST", "localhost"),
+        port=3306,
         database=os.environ.get("PERSONAL_DATA_DB_NAME", "")
     )
     return mydb
