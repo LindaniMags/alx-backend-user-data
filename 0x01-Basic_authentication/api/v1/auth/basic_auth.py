130beb7d0f79c0dc5fb4162e0d5fd8d4f485f59a
LindaniMags <lindanimags@gmail.com>
2024-11-13 00:49:45 +0200

    advanced

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -17,3 +17,40 @@ class BasicAuth(Auth):
         """ Extract base64 authorization header
         """
         pass
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """ Decode base64 authorization header
+        """
+        pass
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """ Extract user credentials
+        """
+        pass
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """ User object from credentials
+        """
+        pass
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current user
+        """
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Require auth
+        """
+        pass
+
+    def authorization_header(self, request=None) -> str:
+        """ Authorization header
+        """
+        pass
