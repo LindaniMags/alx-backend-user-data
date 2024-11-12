221e367fd526b608f503fb6dd86cb26e468b2e5a
LindaniMags <lindanimags@gmail.com>
2024-11-12 22:59:16 +0200

    auth.py

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Auth class
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """ Initialize Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Require Auth
+        """
+        if path in excluded_paths or path == excluded_paths[-1]:
+            return True
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Authorization header
+        """
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current user
+        """
+        return None
