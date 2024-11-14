bdfa5c8fa2268b68813cad10e18d10f031bcde7f
LindaniMags <lindanimags@gmail.com>
2024-11-14 16:13:06 +0200

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """ Basic auth
 """
+import re
+import base64
+import binascii
 from typing import Tuple, TypeVar
 
 from .auth import Auth
@@ -14,6 +17,12 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(
             self,
             authorization_header: str) -> str:
-        """ Extract base64 authorization header
+        """Extracts the Base64 part of the Authorization header
+        for a Basic Authentication.
         """
-        pass
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
